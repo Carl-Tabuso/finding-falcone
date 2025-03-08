00c0b4813294daf3aee14b71d6453d804b0d1808
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-03-08 13:55:48 +0800

    replace global `fetch` with axios

--- services/apiService.js
+++ services/apiService.js
@@ -1,33 +1,36 @@
 import API_URL from '../utils/api.js';
+import axios from 'axios';
 
+/**
+ * TODO: Handle edge-case where api may not return a proper response.
+ * 
+ * Although, it seems unnecessary in this case, hence why i'm
+ * returning the promise w/ the data early, perhaps wrap this in 
+ * a try-catch and return false.
+ */
 export const getApiToken = async () => {
-    return (await fetch(API_URL.TOKEN, {
-        method: "POST",
+    return (await axios.post(API_URL.TOKEN, {}, {
         headers: {
             "Accept": "application/json",
         }
-    }))
-    .json();
+    })).data;
 }
 
 export const getVehicles = async () => {
-    return (await fetch(API_URL.VEHICLES)).json();
+    return (await axios.get(API_URL.VEHICLES)).data;
 }
 
 export const getPlanets = async () => {
-    return (await fetch(API_URL.PLANETS)).json();
+    return (await axios.get(API_URL.PLANETS)).data;
 }
 
 export const findFalcone = async (apiRequestBody) => {
-    return (await fetch(API_URL.FIND, {
-        method: "POST",
+    return (await axios.post(API_URL.FIND, apiRequestBody, {
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json",
         },
-        body: JSON.stringify(apiRequestBody),
-    }))
-    .json();
+    })).data;
 }
 
 export * as apiService from './apiService.js';
