cbfa663ff3dd0de5a906c44305b78fcc6d294117
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-03-09 22:02:10 +0800

    new entry

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import __dirname, { joinPath } from './utils/path.js';
+import { router } from './routes/router.js';
+import nocache from 'nocache';
+
+const app = express();
+const port = 3000;
+
+app.use(nocache());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', router);
+app.use(express.static(joinPath('public')));
+app.set('view engine', 'ejs');
+app.set('views', joinPath('views'));
+app.set('Cache-Control', 'no-store');
+
+app.all('*', (req, res) => {
+    res.status(404);
+    res.send('404 - Not found, go away stupid!');
+});
+
+app.listen(port, () => console.log(`Server listening to port ${port}...`));
+
+export default app;
